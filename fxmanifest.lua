c825e6ff2ef1b80588a8980651af8b92efc0601f
Wiktor <71333177+wiktor-web@users.noreply.github.com>
2023-06-09 19:05:45 +0200

    fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Wiktor Banderas'
-description 'add Weapon'
+description 'Add Weapon'
 
--- Pliki skryptu, które muszą zostać uwzględnione
 client_scripts {
     'client.lua'
 }
@@ -13,8 +12,6 @@ server_scripts {
     'server.lua'
 }
 
--- Możesz dodać inne zależności, takie jak biblioteki lub inne skrypty
 dependencies {
-    'es_extended', -- Przykładowa zależność (zależność od es_extended)
-    -- Dodaj inne zależności tutaj, jeśli są potrzebne
+    'es_extended',
 }
